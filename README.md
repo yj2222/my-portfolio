79b8d870893c1bde552862f288000dc425508fc4
yuji masujima <masjima2712@gmail.com>
2019-08-01 15:48:49 +0900

    Update databace
    データベースのカラム等を変更

--- README.md
+++ README.md
@@ -8,14 +8,38 @@ coming soon...
 
 ## DB設計
 
+## usersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|nickname|string|null: false|
+|email|string|null: false|
+|passward|string|null: false|
+
+### Association
+- has_many :portfolios
+
 ## portfoliosテーブル
 |Column|Type|Options|
 |------|----|-------|
 |title|string|null: false|
-|environment|string|null: false|
+|sub_title|string|null: false|
+|top_image|string|null: false|
+|environment|text|null: false|
+|discription|text|null: false|
+|usage|text|null: false|
+|github_url|string|null: false|
+|app_url|string|null: false|
+|user_id|references|null: false, index: true, foreign_key: true|
+
+### Association
+- belongs_to :user
+- has_many :images
 
 ## imagesテーブル
 |Column|Type|Options|
 |------|----|-------|
-|image|text|null: false|
+|image|string|null: false|
 |portfolio_id|references|null: false,index: true,foreign_key: true|
+
+### Association
+- belongs_to :portfolio
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_07_25_124432) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
+    t.string "image", null: false
     t.bigint "portfolio_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,10 +22,32 @@ ActiveRecord::Schema.define(version: 2019_07_25_124432) do
 
   create_table "portfolios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.string "environment", null: false
+    t.string "sub_title", null: false
+    t.string "top_image", null: false
+    t.text "environment", null: false
+    t.text "discription", null: false
+    t.text "usage", null: false
+    t.string "github_url", null: false
+    t.string "app_url", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "images", "portfolios"
+  add_foreign_key "portfolios", "users"
 end
