93f7d352d07b66b525fbdedafaeee9421c0ab404
yuji masujima <masjima2712@gmail.com>
2019-08-01 15:45:14 +0900

    Create Image_Uploader
    イメージアップローダー導入

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :images, dependent: :delete_all
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+
+  mount_uploader :top_image, ImageUploader
 end
